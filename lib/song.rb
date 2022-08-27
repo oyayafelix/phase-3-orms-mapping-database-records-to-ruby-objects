edeab373765febab62e2d056c23fa6dacc2657af
oyayafelix <oyayafelix@gmail.com>
2022-08-27 14:30:13 +0300

    done lab

--- lib/song.rb
+++ lib/song.rb
@@ -2,45 +2,34 @@ class Song
 
   attr_accessor :name, :album, :id
 
-  def initialize(name:, album:, id: nil)
+  def initialize(name:, album:, id:nil)
     @id = id
     @name = name
     @album = album
   end
 
-  def self.drop_table
-    sql = <<-SQL
-      DROP TABLE IF EXISTS songs
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS songs(
         id INTEGER PRIMARY KEY,
         name TEXT,
         album TEXT
       )
-    SQL
-
+      SQL
+    
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
       INSERT INTO songs (name, album)
-      VALUES (?, ?)
+      VALUES (?,?)
     SQL
 
-    # insert the song
     DB[:conn].execute(sql, self.name, self.album)
 
-    # get the song ID from the database and save it to the Ruby instance
-    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    self.id=DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
-    # return the Ruby instance
     self
   end
 
